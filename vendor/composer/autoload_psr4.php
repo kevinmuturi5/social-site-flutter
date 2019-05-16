8655bebd7e89840e33091b12e65469e973825b99
kevin kevin <kevinmuturu5@gmail.com>
2019-05-16 15:27:07 +0300

    post

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,4 +6,5 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'PostgreSQLTutorial\\' => array($baseDir . '/app'),
 );
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,9 +6,25 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit9e2aa93ab9de4c829b827e487ad66092
 {
+    public static $prefixLengthsPsr4 = array (
+        'P' => 
+        array (
+            'PostgreSQLTutorial\\' => 19,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'PostgreSQLTutorial\\' => 
+        array (
+            0 => __DIR__ . '/../..' . '/app',
+        ),
+    );
+
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
+            $loader->prefixLengthsPsr4 = ComposerStaticInit9e2aa93ab9de4c829b827e487ad66092::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit9e2aa93ab9de4c829b827e487ad66092::$prefixDirsPsr4;
 
         }, null, ClassLoader::class);
     }
