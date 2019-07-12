537c4917fec03056dba0666bd1a67553401b1b81
kevin kevin <kevinmuturu5@gmail.com>
2019-07-12 10:59:31 +0300

    finished code

--- config/config.php
+++ config/config.php
@@ -4,9 +4,10 @@ session_start();
 
 $timezone = date_default_timezone_set("Europe/London");
 
-$con = mysqli_connect("ec2-174-129-240-67.compute-1.amazonaws.com", "nxvaerxycwfrcq
-", "916e1504c7075da675f905ab66a8157175a137fe98b43f22f54294e7344f0a0c
-", "d8re99qld0lkoh"); //Connection variable
+// $con = mysqli_connect("ec2-174-129-240-67.compute-1.amazonaws.com", "nxvaerxycwfrcq
+// ", "916e1504c7075da675f905ab66a8157175a137fe98b43f22f54294e7344f0a0c
+// ", "d8re99qld0lkoh"); //Connection variable
+$con = mysqli_connect("localhost", "root", "", "social"); 
 
 if(mysqli_connect_errno()) 
 {
